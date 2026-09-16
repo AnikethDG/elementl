ede20c6a3bb4dcfe70db61155f11e6def13646d2
Umesh Kumhar <umesh.kumhar@us.mcd.com>
2026-09-17 00:32:59 +0530

    update cloudbuild yaml

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "gcs" {
-    # Non-sensitive application-infrastructure shared state bucket
-    bucket = "gcs-pid-ns-cmn-app-tfstate-iv0b"
-    prefix = "terraform/app-infra/development/pid-nse-dev-core-apps-dz09/infra/state"
-  }
+  backend "gcs" {}
 }
